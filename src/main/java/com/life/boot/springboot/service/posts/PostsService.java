29e76391ac5a27485a96030dea2a74f763755b17
kim <rlaqudgusll@naver.com>
2022-12-02 16:20:50 +0900

    2022.12.01 ::: 조회,수정 api 생성

---
+++ src/main/java/com/life/boot/springboot/service/posts/PostsService.java
@@ -0,0 +1,42 @@
+package com.life.boot.springboot.service.posts;
+
+import com.life.boot.springboot.domain.posts.Posts;
+import com.life.boot.springboot.domain.posts.PostsRepository;
+import com.life.boot.springboot.web.dto.PostsResponseDto;
+import com.life.boot.springboot.web.dto.PostsSaveRequestDto;
+import com.life.boot.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new
+                        IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new
+                        IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
+
+    }
+}
